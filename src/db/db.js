d046a8c9ef7acb79b59c9a83200921d02ee9c7bb
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2025-06-03 21:20:01 -0300

    Att

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ console.log(process.env.DB_USER)
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: "J040R007#321",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
